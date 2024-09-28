98d8b19614a6521bbc4c61b804d48ce4a707d977
Jorge Elías García <alu0101472294@ull.eduu.es>
2024-09-28 18:40:33 +0000

    first commit

--- prueba.cc
+++ prueba.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Introduce un numero: ";
+  std::cout << "Introduce un numero: "; 
   int x{};
   std::cin >> x;
   std::cout << "Introduce otro numero: ";
